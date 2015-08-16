228e99bbc6be724ce03ec1c7105a5f46d52bb4e7
ykozlova <yelenavk@gmail.com>
2015-08-16 14:37:19 -0700

    Update get_it file

--- 05_Ruby_Fundamentals/exercises/get_it.rb
+++ 05_Ruby_Fundamentals/exercises/get_it.rb
@@ -1,6 +1,5 @@
 #1 - Pull the json from the reddit API via http://www.reddit.com/.json
   # - http://mashable.com/stories.json (homework)
-  # - http://digg.com/api/news/popular.json (Homework)
 #2 - Parse it using the JSON library
 #3 - Find the stories based on techniques used in the code_along (max of 25 provided)
 #4 - Create a new story hash out of each story with the following keys :title, :upvotes and :category
