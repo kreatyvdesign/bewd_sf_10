c7e9b9d12906a84b2a2c827a90c4c6c32256c6da
ykozlova <yelena.kozlova@druva.com>
2015-09-14 18:08:22 -0700

    add lesson 11 notes

--- 11_Rails_Intro_Controller/codealong/bookazon_app/app/controllers/books_controller.rb
+++ 11_Rails_Intro_Controller/codealong/bookazon_app/app/controllers/books_controller.rb
@@ -6,26 +6,48 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = get_book
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @book = get_book
   end
 
   def update
+    @book = get_book
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    
   end
 
   private
 
   def book_params
+    params.require(:book).permit(:title, :year_published, :author, :available, :genre, :image_url)
+    #params require this model "book", permit these "book" attributes allowing to be changed
+  end
+
+  def get_book
+    Book.find(params[:id])
   end
 
 end
